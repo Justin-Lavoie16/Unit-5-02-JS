e4db663871c7bdcc05af92fa0e9e9082e3e38115
Justin-Lavoie16 <113055699+Justin-Lavoie16@users.noreply.github.com>
2022-11-21 19:22:29 +0000

    43

--- js/script.js
+++ js/script.js
@@ -1,21 +1,29 @@
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/Unit-5-02-JS/sw.js", {
+    scope: "/Unit-5-02-JS/",
   })
 }
 
-;("use strict")
+"use strict"
 
-function calculate() {
-  const length = parseInt(document.getElementById("pay").value)
-  const width = parseInt(document.getElementById("pay2").value)
+const randomNumber = Math.floor(Math.random() * -6) + -1;
+const random = Math.floor(Math.random() * 6) + 1;
 
-  const area = length * width * 0.18
-  const perimeter = length * width * (1.0 - 0.18)
-  const TAX_RATE = 0.18
+function updateSliderValue(valueFromSlider) {
+  document.getElementById("slider-value").innerHTML = valueFromSlider
+}
 
-  document.getElementById("area").innerHTML =
-    "The governemnt will take: " + area + "$"
-  document.getElementById("perimeter").innerHTML =
-    "your pay will be: " + perimeter + "$"
+/**
+ * This function displays the slider value.
+ */
+function myButtonClicked() {
+  buttonOnChecked = document.getElementById('flash1').checked
+
+  if (buttonOnChecked == true) {
+    document.getElementById("hello-world").innerHTML =
+    "the Value is: " + random + "!"
+  } else {
+    document.getElementById("hello-world").innerHTML =
+    "the Value is: " + randomNumber + "!"
+  }
 }
